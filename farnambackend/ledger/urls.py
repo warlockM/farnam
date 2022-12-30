43ec12bc72d90dc78ef852f0d573f226ff11959c
mayank <mnkraj529@gmail.com>
2022-12-30 22:59:27 +0530

    permission class tested

--- farnambackend/ledger/urls.py
+++ farnambackend/ledger/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path("login", views.login, name="login"),
     path("logout", views.logout, name="logout"),
     path("callback", views.callback, name="callback"),
+    path("test", views.testAuth, name="test")
 ]
--- farnambackend/ledger/views.py
+++ farnambackend/ledger/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from .serializers import *
 from . models import *
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 # 📁 webappexample/views.py -----
 
 import json
@@ -78,3 +79,8 @@ def CreateNewDecision(request):
     if serializer.is_valid():
         serializer.save()
     return Response(serializer.data)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def testAuth(request):
+    return Response("Hello World")
